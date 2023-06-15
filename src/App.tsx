f12c5ca91edb983855804f34107975ed55755e0a
bkdragon0228 <rlaqjarb2067@naver.com>
2023-06-15 16:29:00 +0900

    feat : Gnb 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 import { useRoutes } from 'react-router-dom';
 import { routes } from './routes';
+import Gnb from './components/gnb';
 
 function App() {
   const elem = useRoutes(routes)
 
   return (
     <div className="App">
+      <Gnb />
+      <main>
         {elem}
+      </main>
     </div>
   );
 }
---
+++ src/components/gnb.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styled from '@emotion/styled';
+
+const Gnb = () => {
+    const navigationList = [
+        {label : '시작', path : '/'},
+        {label : '로그인', path : '/signin'},
+        {label : '회원가입', path : '/signup'},
+        {label : '할 일', path : '/todo'}
+    ]
+
+    return (
+        <NavigationBar>
+            {
+                navigationList.map(({label, path }) => (
+                    <Link to={path}>{label}</Link>
+                ))
+            }
+        </NavigationBar>
+    );
+};
+
+export default Gnb;
+
+const NavigationBar = styled.nav`
+    padding: 20px;
+    margin: 0 auto;
+    display: flex;
+    column-gap: 1rem;
+    align-items: center;
+    justify-content: center;
+    border-bottom: 1px solid lightgray;
+`
