5903310eff5878603b4e0bde404da7e9da588ba4
bkdragon0228 <rlaqjarb2067@naver.com>
2023-07-31 19:55:51 +0900

    test : useAuth 테스트 작성

--- src/test/hook/useAuth.test.ts
+++ src/test/hook/useAuth.test.ts
@@ -1,11 +1,35 @@
 import React from 'react';
 
+import { BrowserRouter } from 'react-router-dom';
+
 import { renderHook } from '@testing-library/react';
 
 import useAuth from '../../hook/useAuth';
 
 describe('useAuth', () => {
-  //   const settingTestHook = (option: null | boolean, accessToken: string) => {
-  //     const { result } = renderHook(() => useAuth(option));
-  //   };
+  const settingTestHook = (accessToken: string | null) => {
+    const { result } = renderHook(() => useAuth(accessToken), {
+      wrapper: BrowserRouter,
+    });
+
+    return {
+      result,
+    };
+  };
+
+  context('accessToken이 없으면', () => {
+    it('isLogin이 false', () => {
+      const { result } = settingTestHook(null);
+
+      expect(result.current.isLogin).toBe(false);
+    });
+  });
+
+  context('accessToken이 있으면', () => {
+    it('isLogin이 true', () => {
+      const { result } = settingTestHook('sampleToken');
+
+      expect(result.current.isLogin).toBe(true);
+    });
+  });
 });
